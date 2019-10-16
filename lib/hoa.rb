280d25893cbee2eeb436a567209c753093734619
Will Drougas <wdrougas@gmail.com>
2019-10-16 12:59:34 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,8 +6,5 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  BASE_HOA[:chipmunks] << "Dave"
-  BASE_HOA[:third_earthers] << "Snarf"
-  BASE_HOA[:jetsons] << "Astro"
-  
+  array[:show] << "name"
 end
